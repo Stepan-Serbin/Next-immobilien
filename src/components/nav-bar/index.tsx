a86d4a96a0d837b98101ac769a8202a9602c388f
Alisher Khamidov <a.a.khamidov@yandex.ru>
2026-01-20 13:16:16 +0100

    init

---
+++ src/components/nav-bar/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <Link href="/about">Learn about us</Link>
+      <Link href="/career">Career</Link>
+    </nav>
+  );
+}
