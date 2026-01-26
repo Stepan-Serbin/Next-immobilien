c1e7d05c4cb3112b0c3b78a8ce1c0ca7bb4b627e
Stepan Serbin <stepanserbins@gmail.com>
2026-01-26 09:44:12 +0100

    fix

--- src/components/product-carousel/index.tsx
+++ src/components/product-carousel/index.tsx
@@ -31,7 +31,7 @@ export default function ProductCarousel({ images }: { images: string[] }) {
   }, [api]);
 
   return (
-    <div className="mx-auto max-w-[10rem] sm:max-w-xs">
+    <div className="mx-auto max-w-40 sm:max-w-xs">
       <Carousel setApi={setApi} className="w-full max-w-xs">
         <CarouselContent>
           {images.map((image, index) => (
