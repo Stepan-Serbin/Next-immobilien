f62e33789d7e2c58cd023c344cfc0c412b06e0ef
Stepan Serbin <stepanserbins@gmail.com>
2026-01-28 11:47:04 +0100

    fix and update

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,19 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 export const postsTable = pgTable("posts", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: varchar({ length: 255 }).notNull(),
   text: varchar({ length: 255 }).notNull(),
 });
+
+export const reviewsTable = pgTable("reviews", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  content: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+
+
+// npm run db:generate - сгенерирует файл с миграциями SQL
+// npm run db:migrate
