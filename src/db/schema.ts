18d502ac5bc40ed4a99bcdc517a650ce7013dd42
Stepan Serbin <stepanserbins@gmail.com>
2026-01-29 13:09:49 +0100

    update files

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 
-import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
 export const postsTable = pgTable("posts", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: varchar({ length: 255 }).notNull(),
@@ -17,3 +17,23 @@ export const reviewsTable = pgTable("reviews", {
 
 // npm run db:generate - сгенерирует файл с миграциями SQL
 // npm run db:migrate
+
+export const users = pgTable("users", {
+  id: serial().primaryKey(),
+  name: varchar({ length: 250 }).notNull(),
+  email: varchar({ length: 250 }).notNull().unique(),
+  image: text(),
+  role: varchar({ length: 100 }).default("customer"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const todos = pgTable("todos", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 250 }).notNull(),
+  description: varchar({ length: 250 }).notNull(),
+  status: boolean().default(false),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+});
